f6a8a75084d550f658a3ae7723b634714d10bdb9
Bruno Santos <83880794+bruno34154@users.noreply.github.com>
2024-04-07 22:56:19 -0300

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../screens/Login/Login';
+import Feed from '../screens/Profile/Profile';
 import Home from '../screens/Home/Home';
 import ManutencaoScreen from '../screens/Maintenance/Maintenance';
 import RegistroProfessor from '../screens/RegistrationTeacher/RegistrationTeacher';
@@ -14,17 +15,14 @@ import Space from '../screens/Space/Space';
 
 const Stack = createStackNavigator();
 
-
 const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{headerShown: false}}>
-
-
         {/* // Login */}
-         <Stack.Screen name="Login" component={Login} /> 
+        <Stack.Screen name="Login" component={Login} />
 
         {/* Menu de beixo */}
 
@@ -37,7 +35,7 @@ const Routes = () => {
         <Stack.Screen name="Space" component={Space} />
 
         {/* Perfil */}
-        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Profile" component={Feed} />
 
         {/* Manutenção de salas */}
         <Stack.Screen name="Maintenance" component={ManutencaoScreen} />
@@ -49,14 +47,12 @@ const Routes = () => {
           component={RegistroProfessor}
         />
 
-
         {/* Cadastrar Gestor ao espáço */}
         <Stack.Screen name="LinkManagersSpace" component={LinkManagersSpace} />
-        
+
         {/* Cadastrar Setor */}
 
         <Stack.Screen name="RegistrationSector" component={RegistarSetor} />
-        <Stack.Screen name="Profile" component={Feed} />
       </Stack.Navigator>
     </NavigationContainer>
   );
